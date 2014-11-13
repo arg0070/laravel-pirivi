1ee6642a9b5564f1df12aa82586b17db6f474ab4
arg0070 <arg0070@alu.ubu.es>
2014-11-13 12:59:25 +0100

    Minimal templates changes

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,66 +1,54 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    @section('title')
-        <title>{{{$title}}}</title> <!-- using {{$title}} is bad here eg:</title><script>alert('hello')</script> -->
-    @show
-    {{ HTML::style('assets/css/foundation/foundation.css') }}
-    {{ HTML::style('assets/css/font-awesome/font-awesome.css') }}   
-</head>
-<body>
-<div class="row main">
-    <div class="small-12 large-12 column" id="masthead">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        @section('title')
+            <title>{{{$title}}}</title> <!-- using {{$title}} is bad here eg:</title><script>alert('hello')</script> -->
+        @show
+        {{ HTML::style('assets/css/foundation/foundation.css') }}
+        {{ HTML::style('assets/css/custom.css') }}   
+        {{ HTML::style('assets/css/font-awesome/font-awesome.css') }}   
+    </head>
+    <body>    
         <header>
-            <nav class="top-bar" data-topbar>
+            <nav class="top-bar" data-topbar role="navigation">                                    
                 <ul class="title-area">
-                    <!-- Title Area -->
-                    <li class="name"></li>
+                    <li class="name">
+                        <h1><a href="#">{{HTML::image('media/logo.jpg')}} Piriví</a></h1>
+                    </li>
                     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
                     <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
                 </ul>
                 <section class="top-bar-section">
-                    <ul class="left">
-                        <li class="{{(strcmp(URL::full(), URL::to('/')) == 0) ? 'active' : ''}}"><a href="{{URL::to('/')}}">Home</a></li>
-                    </ul>
+                    <!-- Right Nav Section -->
                     <ul class="right">
-                        @if(Auth::check())
-                            <li class="{{ (strpos(URL::current(), URL::to('admin/dash-board'))!== false) ? 'active' : '' }}">
-                                {{HTML::link('admin/dash-board','Dashboard')}}
-                            </li>
-                            <li class="{{ (strpos(URL::current(), URL::to('logout'))!== false) ? 'active' : '' }}" >
-                                {{HTML::link('logout','Logout')}}
-                            </li>
-                        @else
-                            <li class="{{ (strpos(URL::current(), URL::to('login'))!== false) ? 'active' : '' }}">
-                                {{HTML::link('login','Login')}}
-                            </li>
-                        @endif
-                    </ul>
-                </section>
+                        <li class="active"><a href="#">Right Button Active</a></li>
+                        <li class="has-dropdown">
+                            <a href="#">Right Button Dropdown</a>
+                            <ul class="dropdown">
+                                <li><a href="#">First link in dropdown</a></li>
+                                <li class="active"><a href="#">Active link in dropdown</a></li>
+                            </ul>
+                        </li>
+                    </ul>                                                       
+                </section>                
             </nav>
-            <div class="sub-header">
-                <hgroup>
-                    <h1>{{HTML::link('/','Laravel 4 Blog')}}</h1>
-                    <h2>Laravel 4 Simple Blog tutorial</h2>
-                </hgroup>
-            </div>
+            {{$subheader}}            
         </header>
-    </div>
-    <div class="row">
-        {{$main}}
-    </div>
-    <div class="row">
-        <div class="small-12 large-12 column">
-            <footer class="site-footer"></footer>
+
+        <div class="row">
+            {{$main}}
         </div>
-    </div>
-</div>
-{{ HTML::script('assets/js/jquery/jquery.js') }}
-{{ HTML::script('assets/js/foundation/foundation.js') }}
-<script>
-    $(document).foundation();
-</script>
-</body>
+        <div class="row">
+            <div class="small-12 large-12 column">
+                <footer class="site-footer"></footer>
+            </div>
+        </div>
+        {{ HTML::script('assets/js/jquery/jquery.js') }}
+        {{ HTML::script('assets/js/foundation/foundation.js') }}
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
 </html>
