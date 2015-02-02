37e9c019b8243347417897f0b36f410885840963
arg0070 <arg0070@alu.ubu.es>
2015-02-02 11:02:02 +0100

    Warning message

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -8,7 +8,7 @@
             <form>
                 <div class="row collapse postfix-round">
                     <div class="small-10 columns">
-                        <input type="text" placeholder="Buscar objetos"> 
+                        <input type="text" placeholder="Buscar objetos">
                     </div>
                     <div class="small-2 columns">
                         <a href="#" class="button postfix"><i class="fa fa-search fa-2x"></i></a>
@@ -18,17 +18,17 @@
         </div>
 
         <div class="large-4 columns points">
-            <i class="fa fa-map-marker fa-4x"></i>                   
+            <i class="fa fa-map-marker fa-4x"></i>
             <h3>Alquiler cercano</h3>
             <p>Encuentra lo que buscas cerca de ti</p>
         </div>
-        <div class="large-4 columns points">    
-            <i class="fa fa-euro fa-4x"></i>                     
+        <div class="large-4 columns points">
+            <i class="fa fa-euro fa-4x"></i>
             <h3>Precios racionales</h3>
             <p>Prueba antes de comprar</p>
         </div>
         <div class="large-4 columns points">
-            <i class="fa fa-lock fa-4x"></i>                   
+            <i class="fa fa-lock fa-4x"></i>
             <h3>Seguridad y confianza</h3>
             <p>Sistema de reputación verificado</p>
         </div>
--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -7,63 +7,72 @@
             <title>{{{$title}}}</title> <!-- using {{$title}} is bad here eg:</title><script>alert('hello')</script> -->
         @show
         {{ HTML::style('assets/css/foundation/foundation.css') }}
-        {{ HTML::style('assets/css/custom.css') }}   
-        {{ HTML::style('assets/css/font-awesome/font-awesome.css') }}   
+        {{ HTML::style('assets/css/custom.css') }}
+        {{ HTML::style('assets/css/font-awesome/font-awesome.css') }}
     </head>
-    <body>    
+    <body>
 
-        <div class="off-canvas-wrap" data-offcanvas> 
+        <div class="off-canvas-wrap" data-offcanvas>
             <div class="inner-wrap">
                 <header id="main-menu">
                     <div class="row">
-                        <nav class="tab-bar"> 
-                            <section class="left-small"> 
-                                <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a> 
-                            </section> 
-                            <section class="middle tab-bar-section"> 
-
-                                <h1>{{HTML::image('media/logo.jpg')}} {{HTML::link('/','Piriví')}}</h1>                        
+                        <nav class="tab-bar">
+                            <section class="left-small">
+                                <a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
                             </section>
-                            <section class="right">                                                                           
-                                <a href="#"><i class="fa fa-search fa-2x"></i></a>
-                            </section> 
+                            <section class="middle tab-bar-section">
 
-                        </nav> 
+                                <h1>{{HTML::image('media/logo.jpg')}} {{HTML::link('/','Piriví')}}</h1>
+                            </section>
+                            <section class="right">
+                                <a href="#"><i class="fa fa-search fa-2x"></i></a>
+                            </section>
+
+                        </nav>
+
                     </div>
                 </header>
+                <div class="alert alert-warning">
+                  <div class="row">
+                    <div class="large-12 columns">
+                      <p class="text-center"><strong>AVISO</strong>: esta página es un <strong>PROTOTIPO NO OPERATIVO</strong> que
+                      no muestra el resultado final de la misma.</p>
+                    </div>
+                  </div>
+                </div>
 
-                <aside class="left-off-canvas-menu"> 
-                    <ul class="off-canvas-list"> 
-                        <li><label>Menú</label></li> 
-                        <li><a href="#"><i class="fa fa-home fa-fw"></i> Inicio</a></li> 
+                <aside class="left-off-canvas-menu">
+                    <ul class="off-canvas-list">
+                        <li><label>Menú</label></li>
+                        <li><a href="#"><i class="fa fa-home fa-fw"></i> Inicio</a></li>
                         <li><a href="#"><i class="fa fa-user fa-fw"></i> Cuenta</a></li>
-                        <li><a href="#"><i class="fa fa-newspaper-o fa-fw"></i> Mis anuncios</a></li> 
+                        <li><a href="#"><i class="fa fa-newspaper-o fa-fw"></i> Mis anuncios</a></li>
                         <li><a href="#"><i class="fa fa-bars fa-fw"></i> Mis alquileres</a></li>
-                        <li><a href="#"><i class="fa fa-shopping-cart fa-fw"></i> Lista de deseados</a></li> 
-                    </ul> 
-                    <ul class="off-canvas-list"> 
-                        <li><label>Categorías</label></li> 
-                        <li><a href="#">Tecnología <span class="success radius label right">2050</span></a></li>
-                        <li><a href="#">Deporte <span class="secondary radius label right">1680</span></a></li>                       
+                        <li><a href="#"><i class="fa fa-shopping-cart fa-fw"></i> Lista de deseados</a></li>
                     </ul>
-                    <ul class="off-canvas-list"> 
-                        <li><label>Ciudades</label></li> 
+                    <ul class="off-canvas-list">
+                        <li><label>Categorías</label></li>
+                        <li><a href="#">Tecnología <span class="success radius label right">2050</span></a></li>
+                        <li><a href="#">Deporte <span class="secondary radius label right">1680</span></a></li>
+                    </ul>
+                    <ul class="off-canvas-list">
+                        <li><label>Ciudades</label></li>
                         <li><input type="text" placeholder="Población o CP"></li>
                         <li><a href="#">Burgos </a></li>
                         <li><a href="#">Valladolid </a></li>
-                        <li><a href="#">Madrid </a></li> 
-                        <li><a href="#"><b>. . .</b> </a></li> 
+                        <li><a href="#">Madrid </a></li>
+                        <li><a href="#"><b>. . .</b> </a></li>
                     </ul>
                     <ul class="off-canvas-list">
                         <li><label>Información</label></li>
                         <li><a href="#">Sobre nosotros</a></li>
                         <li><a href="#">Información legal</a></li>
                         <li><a href="#">Condiciones del servicio</a></li>
-                        <li><a href="#">Centro se soporte</a></li>                                                                    
+                        <li><a href="#">Centro se soporte</a></li>
                     </ul>
-                </aside> 
+                </aside>
                 <section class="main-section">
-                              
+
 
                     {{$main}}
 
@@ -80,7 +89,7 @@
                                     <li><span class="fa fa-google-plus-square fa-2x"></span></li>
                                 </ul>
                             </div>
-                            <div class="small-12 large-8 column end">                
+                            <div class="small-12 large-8 column end">
                                 <ul class="inline-list">
                                     <li><a href="#">Sobre nosotros</a></li>
                                     <li><a href="#">Información legal</a></li>
@@ -93,7 +102,7 @@
 
                         </div>
                     </footer>
-                </section> 
+                </section>
                 <a class="exit-off-canvas"></a> </div> </div>
 
         {{ HTML::script('assets/js/jquery/jquery.js') }}
